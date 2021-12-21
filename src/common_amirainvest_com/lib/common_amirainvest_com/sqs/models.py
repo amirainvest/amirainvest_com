42b5237025f072ee0f78f7b51f1f04ee42defcf6
Peter Hoburg <peterHoburg@users.noreply.github.com>
2021-12-20 23:38:18 -0700

    common repo commit: f0ac34854cbebc7979dc4210a32396d2dfe68d58 latest on: dj/brokerage-sqs-models

--- src/common_amirainvest_com/lib/common_amirainvest_com/sqs/models.py
+++ src/common_amirainvest_com/lib/common_amirainvest_com/sqs/models.py
@@ -1,6 +1,35 @@
-from pydantic import BaseModel
+from enum import Enum
+from typing import Optional
+
+from arrow import Arrow
+from pydantic import BaseModel, validator
 
 
 class MediaPlatformDataLoadQueueModel(BaseModel):
     platform: str
     unique_platform_id: str
+
+
+class BrokerageDataActions(Enum):
+    holdings_change = "holdings_change"
+    investments_change = "investments_change"
+
+
+class Brokerage(Enum):
+    plaid = "plaid"
+
+
+class BrokerageDataChange(BaseModel):
+    brokerage: Brokerage
+    brokerage_user_id: str
+    action: BrokerageDataActions
+    start_date: Optional[Arrow]
+    end_date: Optional[Arrow]
+
+    @validator("start_date")
+    def format_start_date(cls, value):
+        return value._datetime
+
+    @validator("end_date")
+    def format_end_date(cls, value):
+        return value._datetime

af62806510849d8c31fd5b663e72e0f740513617
Peter Hoburg <peterHoburg@users.noreply.github.com>
2021-12-26 20:45:27 -0700

    #27: Added tests for all routes on app. Tests check if there is auth.

---
+++ src/backend_amirainvest_com/test/unit/backend_amirainvest_com/api/routers/test_auth.py
@@ -0,0 +1,53 @@
+pytest_plugins = ["common_amirainvest_com.utils.test.fixtures.database"]
+
+import pytest
+from fastapi.security import HTTPBearer
+from httpx import AsyncClient
+
+from backend_amirainvest_com.api.app import app
+
+from .config import AUTH_HEADERS
+
+
+routes_with_no_auth_required_path = [
+    "/openapi.json",
+    "/docs",
+    "/docs/oauth2-redirect",
+    "/redoc",
+    "/admin/health_check",
+    "/code_challenge/",
+    "/",
+]
+
+
+# TODO figure out why removing the auth decorator will pass the tests, be removing the token will fail them
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("route", app.routes)
+async def test_not_authenticated_get_user(route):
+    print(route.name)
+    try:
+        dependant = route.dependant
+        dependencies = dependant.dependencies
+
+        if len(dependencies) == 0:
+            assert route.path in routes_with_no_auth_required_path
+        for method in route.methods:
+            for dep in dependencies:
+                if dep.name == "token":
+                    assert type(dep.call) == HTTPBearer
+                    async with AsyncClient(app=app, base_url="http://test") as async_client:
+                        client_attr = getattr(async_client, method.lower())
+
+                        no_auth_response = await client_attr(route.path)
+                        assert no_auth_response.status_code == 403
+                        assert no_auth_response.json() == {"detail": "Not authenticated"}
+
+                        auth_response = await client_attr(route.path, headers=AUTH_HEADERS)
+                        assert auth_response.status_code in {200, 422}
+                    break
+            else:
+                assert route.path in routes_with_no_auth_required_path
+    except AttributeError:
+        assert route.path in routes_with_no_auth_required_path

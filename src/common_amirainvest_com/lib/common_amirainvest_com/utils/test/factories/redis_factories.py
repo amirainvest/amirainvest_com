7676c397032cc8325e78fb40b29ad5e9fdc74dd9
Peter Hoburg <peterHoburg@users.noreply.github.com>
2021-12-28 23:34:41 -0700

    #53: changed file name, changed generated data type.

---
+++ src/common_amirainvest_com/lib/common_amirainvest_com/utils/test/factories/redis_factories.py
@@ -0,0 +1,17 @@
+from hypothesis import given, strategies as st
+
+from backend_amirainvest_com.models.feed import FeedType
+from common_amirainvest_com.schemas.schema import PostsModel
+from common_amirainvest_com.utils.consts import WEBCACHE
+
+
+# TODO figure out how to generate all non None data.
+@given(st.builds(PostsModel))
+def posts_redis_factory(user_id: str, feed_type: str, posts_model: PostsModel):
+    key = f"{user_id}-{feed_type}"
+    post_json = posts_model.json()
+    WEBCACHE.lpush(key, post_json)
+
+
+if __name__ == '__main__':
+    posts_redis_factory("fake_user_id", FeedType.subscriber)

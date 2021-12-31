08ed26c81195038ff790e54d6c6ff9adeaa07f0a
Peter Hoburg <peterHoburg@users.noreply.github.com>
2021-12-30 22:52:32 -0700

    #53: Added some tests, Removed some redundant checks.

--- src/backend_amirainvest_com/test/unit/backend_amirainvest_com/api/routers/test_feed.py
+++ src/backend_amirainvest_com/test/unit/backend_amirainvest_com/api/routers/test_feed.py
@@ -18,22 +18,51 @@ async def test_get_subscriber_feed():
     creator = await UsersFactory()
     subscriber = await UsersFactory()
     await UserSubscriptionsFactory(subscriber_id=subscriber.id, creator_id=creator.id, is_deleted=False)
+
     for _ in range(0, 3):
         post = await PostsFactory(creator_id=creator.id, id=randint(0, 10000))
         posts_redis_factory(creator.id, "subscriber", PostsModel(**{k: v for k, v in post.__dict__.items()}))
+
     async with AsyncClient(app=app, base_url="http://test") as async_client:
         response = await async_client.get(
             "/feed/subscriber/", headers=AUTH_HEADERS, params={"subscriber_id": subscriber.id}
         )
-    assert response.status_code == 200
+
     response_data = response.json()
+
+    assert response.status_code == 200
+
     assert type(response_data) == dict
-    assert "feed_type" in response_data
-    assert response_data["feed_type"] == "subscriber"
-    assert "posts" in response_data
     assert type(response_data["posts"]) == list
+
+    assert response_data["feed_type"] == "subscriber"
+
     assert len(response_data["posts"]) == 3
-    assert response_data["posts"][0]["creator_id"] == str(creator.id)
+    assert all([response["creator_id"] == str(creator.id) for response in response_data["posts"]])
+
+
+@pytest.mark.asyncio
+async def test_get_empty_subscriber_feed():
+    creator = await UsersFactory()
+    subscriber = await UsersFactory()
+    await UserSubscriptionsFactory(subscriber_id=subscriber.id, creator_id=creator.id, is_deleted=False)
+
+    async with AsyncClient(app=app, base_url="http://test") as async_client:
+        response = await async_client.get(
+            "/feed/subscriber/", headers=AUTH_HEADERS, params={"subscriber_id": subscriber.id}
+        )
+
+    response_data = response.json()
+
+    assert response.status_code == 200
+
+    assert type(response_data) == dict
+    assert type(response_data["posts"]) == list
+
+    assert response_data["feed_type"] == "discovery"
+
+    assert len(response_data["posts"]) == 0
+    assert all([response["creator_id"] == str(creator.id) for response in response_data["posts"]])
 
 
 @pytest.mark.asyncio
@@ -41,40 +70,73 @@ async def test_get_creator_feed():
     creator = await UsersFactory()
     subscriber = await UsersFactory()
     await UserSubscriptionsFactory(creator_id=creator.id, subscriber_id=subscriber.id)
+
     for _ in range(0, 3):
         post = await PostsFactory(creator_id=creator.id, id=randint(0, 10000))
         post_model = PostsModel(**{k: v for k, v in post.__dict__.items()})
         posts_redis_factory(creator.id, "creator", post_model)
+
     async with AsyncClient(app=app, base_url="http://test") as async_client:
         response = await async_client.get("/feed/creator/", headers=AUTH_HEADERS, params={"creator_id": creator.id})
-    assert response.status_code == 200
+
     response_data = response.json()
+
+    assert response.status_code == 200
+
     assert type(response_data) == dict
-    assert "feed_type" in response_data
-    assert response_data["feed_type"] == "creator"
-    assert "posts" in response_data
     assert type(response_data["posts"]) == list
+
+    assert response_data["feed_type"] == "creator"
+
     assert len(response_data["posts"]) == 3
-    assert response_data["posts"][0]["creator_id"] == str(creator.id)
+    assert all([response["creator_id"] == str(creator.id) for response in response_data["posts"]])
 
 
 @pytest.mark.asyncio
-async def test_get_discovery_feed():
+async def test_get_empty_creator_feed():
     creator = await UsersFactory()
     subscriber = await UsersFactory()
     await UserSubscriptionsFactory(creator_id=creator.id, subscriber_id=subscriber.id)
-    for _ in range(0, 3):
+
+    async with AsyncClient(app=app, base_url="http://test") as async_client:
+        response = await async_client.get("/feed/creator/", headers=AUTH_HEADERS, params={"creator_id": creator.id})
+
+    response_data = response.json()
+
+    assert response.status_code == 200
+
+    assert type(response_data) == dict
+    assert type(response_data["posts"]) == list
+
+    assert response_data["feed_type"] == "discovery"
+
+    assert len(response_data["posts"]) == 0
+    assert all([response["creator_id"] == str(creator.id) for response in response_data["posts"]])
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("number_of_posts", [0, 10])
+async def test_get_discovery_feed(number_of_posts: int):
+    creator = await UsersFactory()
+    subscriber = await UsersFactory()
+    await UserSubscriptionsFactory(creator_id=creator.id, subscriber_id=subscriber.id)
+
+    for _ in range(0, number_of_posts):
         post = await PostsFactory(creator_id=creator.id, id=randint(0, 10000))
         post_model = PostsModel(**{k: v for k, v in post.__dict__.items()})
         posts_redis_factory(creator.id, "discovery", post_model)
+
     async with AsyncClient(app=app, base_url="http://test") as async_client:
         response = await async_client.get("/feed/discovery/", headers=AUTH_HEADERS, params={"user_id": subscriber.id})
-    assert response.status_code == 200
+
     response_data = response.json()
+
+    assert response.status_code == 200
+
     assert type(response_data) == dict
-    assert "feed_type" in response_data
-    assert response_data["feed_type"] == "discovery"
-    assert "posts" in response_data
     assert type(response_data["posts"]) == list
-    assert len(response_data["posts"]) == 3
-    assert response_data["posts"][0]["creator_id"] == str(creator.id)
+
+    assert response_data["feed_type"] == "discovery"
+
+    assert len(response_data["posts"]) == number_of_posts
+    assert all([response["creator_id"] == str(creator.id) for response in response_data["posts"]])

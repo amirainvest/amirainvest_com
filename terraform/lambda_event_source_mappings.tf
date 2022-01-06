c0cfbff89e88216002459483403894d3c343b000
Peter Hoburg <peterHoburg@users.noreply.github.com>
2022-01-05 19:47:04 -0700

    Added lambda function

---
+++ terraform/lambda_event_source_mappings.tf
@@ -0,0 +1,12 @@
+//resource "aws_lambda_event_source_mapping" "sqs_to_test_stuff_and_things" {
+//  batch_size                         = "10"
+//  bisect_batch_on_function_error     = "false"
+//  enabled                            = "true"
+//  event_source_arn                   = "arn:aws:sqs:us-east-1:903791206266:prod-brokerage-data"
+//  function_name                      = aws_lambda_function.testing_stuff_and_things.arn
+//  maximum_batching_window_in_seconds = "0"
+//  maximum_record_age_in_seconds      = "-1"
+//  maximum_retry_attempts             = "3"
+//  parallelization_factor             = "10"
+//  tumbling_window_in_seconds         = "0"
+//}
---
+++ terraform/lambda_functions.tf
@@ -0,0 +1,37 @@
+resource "aws_lambda_function" "testing_stuff_and_things" {
+  architectures = ["x86_64"]
+
+  environment {
+    variables = {
+      ENVIRONMENT = "prod"
+      PROJECT     = "brokerage"
+    }
+  }
+
+  function_name                  = "testing_stuff_and_things"
+  image_uri                      = "${aws_ecr_repository.lambda.repository_url}@${data.aws_ecr_image.lambda.id}"
+  memory_size                    = "256"
+  package_type                   = "Image"
+  reserved_concurrent_executions = "-1"
+  role                           = "arn:aws:iam::903791206266:role/prod-lambda"
+  source_code_hash               = "b03f25362d49e350017d0f2707efdf2e1d61ba2acbad1489ee704df50a863c95"
+
+  tags = {
+    env = "prod"
+  }
+
+  tags_all = {
+    env = "prod"
+  }
+
+  timeout = "900"
+
+  tracing_config {
+    mode = "PassThrough"
+  }
+
+  vpc_config {
+    security_group_ids = ["sg-05e6b9e1e61a849f4"]
+    subnet_ids         = ["subnet-031bdf1a786694a68", "subnet-05a45c47337ce649e", "subnet-0bb4370ccc2df1e3e"]
+  }
+}

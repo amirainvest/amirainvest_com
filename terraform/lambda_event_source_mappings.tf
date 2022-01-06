d67c146edba0687a16e353570049d988321e908b
Peter Hoburg <peterHoburg@users.noreply.github.com>
2022-01-05 19:50:51 -0700

    Adjusted the lambda_event_source_mappings.tf

--- terraform/lambda_event_source_mappings.tf
+++ terraform/lambda_event_source_mappings.tf
@@ -5,8 +5,5 @@ resource "aws_lambda_event_source_mapping" "sqs_to_test_stuff_and_things" {
   event_source_arn                   = "arn:aws:sqs:us-east-1:903791206266:prod-brokerage-data"
   function_name                      = aws_lambda_function.testing_stuff_and_things.arn
   maximum_batching_window_in_seconds = "0"
-  maximum_record_age_in_seconds      = "-1"
-  maximum_retry_attempts             = "3"
-  parallelization_factor             = "10"
   tumbling_window_in_seconds         = "0"
 }

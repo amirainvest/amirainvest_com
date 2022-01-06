aa01d51887cff7a1cb82ab1622bffcae5e5ae237
Peter Hoburg <peterHoburg@users.noreply.github.com>
2022-01-06 15:46:52 -0700

    Added poetry install step.

--- bin/run_alembic_migrations.sh
+++ bin/run_alembic_migrations.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+poetry install --remove-untracked
 (export ENVIRONMENT="$1" && eval "$(poetry run python -m common_amirainvest_com.utils.load_secrets)" && poetry run alembic upgrade head)

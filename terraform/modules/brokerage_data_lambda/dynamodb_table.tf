d0e0b747d238e2aabc35e613dd96c3dd11e3bebc
Peter Hoburg <peterHoburg@users.noreply.github.com>
2022-01-08 22:01:12 -0700

    Added brokerage_users dynamo table to TF

---
+++ terraform/modules/brokerage_data_lambda/dynamodb_table.tf
@@ -0,0 +1,37 @@
+resource "aws_dynamodb_table" "brokerage_users" {
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "user_id"
+  name = "brokerage_users"
+
+  point_in_time_recovery {
+    enabled = "false"
+  }
+
+  stream_enabled = "false"
+  table_class = "STANDARD"
+
+  tags = {
+    env = "prod"
+    project = "brokerage"
+  }
+
+  tags_all = {
+    env = "prod"
+    project = "brokerage"
+  }
+
+  read_capacity = "0"
+  write_capacity = "0"
+
+  lifecycle {
+    ignore_changes = [
+      read_capacity,
+      write_capacity,
+    ]
+  }
+}

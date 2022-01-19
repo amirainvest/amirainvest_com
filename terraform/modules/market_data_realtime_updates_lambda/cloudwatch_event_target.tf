d5391bed9041777ac80075f9fbc1f71e1af4eee1
Peter Hoburg <peterHoburg@users.noreply.github.com>
2022-01-19 10:28:12 -0700

    Changed cron

--- terraform/modules/market_data_realtime_updates_lambda/cloudwatch_event_target.tf
+++ terraform/modules/market_data_realtime_updates_lambda/cloudwatch_event_target.tf
@@ -1,5 +1,4 @@
 resource "aws_cloudwatch_event_target" "working_day_every_minute_cron" {
   arn       = module.market_data_realtime_updates_lambda.lambda-arn
   rule      = aws_cloudwatch_event_rule.working_day_every_minute_cron.name
-  target_id = "45a3f022-5b02-4dc5-b2ac-8a8d3ff8c354"
 }

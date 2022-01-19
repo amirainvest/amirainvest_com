8cd14ff7bedf108a2f2f86aaf1e468f7e92f7981
Peter Hoburg <peterHoburg@users.noreply.github.com>
2022-01-19 16:29:25 -0700

    Wasn't using var...

--- terraform/modules/lambda_base/lambda_functions.tf
+++ terraform/modules/lambda_base/lambda_functions.tf
@@ -12,7 +12,7 @@ resource "aws_lambda_function" "main" {
   image_uri                      = "${aws_ecr_repository.lambda.repository_url}@${data.aws_ecr_image.lambda.id}"
   memory_size                    = "256"
   package_type                   = "Image"
-  reserved_concurrent_executions = "-1"
+  reserved_concurrent_executions = var.lambda_reserved_concurrent_executions
   role                           = aws_iam_role.lambda.arn
 
 

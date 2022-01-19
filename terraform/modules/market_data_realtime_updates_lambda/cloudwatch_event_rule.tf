f9982afc0915f265c79e2bbc76543b48ec9fd608
Peter Hoburg <peterHoburg@users.noreply.github.com>
2022-01-19 11:16:15 -0700

    Fixed cron

--- terraform/modules/market_data_realtime_updates_lambda/cloudwatch_event_rule.tf
+++ terraform/modules/market_data_realtime_updates_lambda/cloudwatch_event_rule.tf
@@ -3,7 +3,7 @@ resource "aws_cloudwatch_event_rule" "working_day_every_minute_cron" {
   event_bus_name      = "default"
   is_enabled          = "true"
   name                = "${local.lambda_name}_working_day_every_minute_cron"
-  schedule_expression = "cron(0/1 8-17 ? * MON-FRI *)"
+  schedule_expression = "cron(0/1 13-22 ? * MON-FRI *)"
 }
 
 resource "aws_lambda_permission" "allow_working_day_every_minute_cron_to_call_market_data_realtime_updates_lambda" {

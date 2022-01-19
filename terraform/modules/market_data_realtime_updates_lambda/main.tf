2332ebf05306c47909500ba1685691761a5ea9e7
Peter Hoburg <peterHoburg@users.noreply.github.com>
2022-01-18 20:17:11 -0700

    More moved

--- terraform/modules/market_data_realtime_updates_lambda/main.tf
+++ terraform/modules/market_data_realtime_updates_lambda/main.tf
@@ -13,3 +13,33 @@ moved {
   from = aws_cloudwatch_log_group.lambda
   to = module.market_data_realtime_updates_lambda.aws_cloudwatch_log_group.lambda
 }
+
+moved {
+  from = aws_ecr_repository.lambda
+  to = module.market_data_realtime_updates_lambda.aws_ecr_repository.lambda
+}
+
+moved {
+  from = aws_iam_policy.log-writing
+  to = module.market_data_realtime_updates_lambda.aws_iam_policy.log-writing
+}
+
+moved {
+  from = aws_iam_role.lambda
+  to = module.market_data_realtime_updates_lambda.aws_iam_role.lambda
+}
+
+moved {
+  from = aws_kms_key.logs
+  to = module.market_data_realtime_updates_lambda.aws_kms_key.logs
+}
+
+moved {
+  from = aws_lambda_function.realtime_updates
+  to = module.market_data_realtime_updates_lambda.aws_lambda_function.main
+}
+
+moved {
+  from = aws_security_group.lambda
+  to = module.market_data_realtime_updates_lambda.aws_security_group.lambda
+}

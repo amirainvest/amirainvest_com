0b22f3fcfa4491c078c4e5c7e6b862345fef50a6
mountain <Fiveoat@gmail.com>
2022-01-22 12:44:07 -0700

    #144: added return type

--- src/backend_amirainvest_com/lib/backend_amirainvest_com/api/backend/user_feedback/controller.py
+++ src/backend_amirainvest_com/lib/backend_amirainvest_com/api/backend/user_feedback/controller.py
@@ -8,7 +8,7 @@ from common_amirainvest_com.utils.decorators import Session
 
 
 @Session
-async def create_controller(session: AsyncSession, user_id: UUID, user_feedback_data: CreateModel):
+async def create_controller(session: AsyncSession, user_id: UUID, user_feedback_data: CreateModel) -> UserFeedback:
     feedback_data = user_feedback_data.dict(exclude_none=True)
     feedback_data["user_id"] = user_id
     user_feedback = UserFeedback(**feedback_data)

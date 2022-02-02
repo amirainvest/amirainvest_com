ed4f33373769fae0dc83a124b84b5a4387f15cd4
jhirschibar <jhirschibar@gmail.com>
2022-02-02 17:01:17 -0500

    update pydantic models

--- src/backend_amirainvest_com/lib/backend_amirainvest_com/api/backend/notifications/router.py
+++ src/backend_amirainvest_com/lib/backend_amirainvest_com/api/backend/notifications/router.py
@@ -10,8 +10,8 @@ from backend_amirainvest_com.api.backend.notifications.controller import (
     update_settings_controller
 )
 from backend_amirainvest_com.api.backend.notifications.model import (
-    CreateModel, GetModel, UpdateModel,
-    CreateSettingsModel, GetSettingsModel, UpdateSettingsModel
+    CreateModel, NotificationsModel, UpdateModel,
+    CreateSettingsModel, NotificationSettingsModel, UpdateSettingsModel
 )
 from backend_amirainvest_com.controllers.auth import auth_dep, auth_depends_user_id
 
@@ -24,7 +24,7 @@ router = APIRouter(prefix="/notifications", tags=["Notifications / Settings"] )#
 @router.post("/get", 
     summary='Retrieve User Notifications', 
     description='This should run when you initialize the Notifications tab', status_code=status.HTTP_200_OK, 
-    response_model=GetModel
+    response_model=NotificationsModel
     )
 async def get_route(user_id: uuid.UUID):
     return (
@@ -36,7 +36,7 @@ async def get_route(user_id: uuid.UUID):
     summary='Update User Notifications', 
     description='Use this to delete a notification or to mark it as read',
     status_code=status.HTTP_200_OK, 
-    response_model=GetModel
+    response_model=NotificationsModel
     )
 async def update_route(user_id: uuid.UUID, update_data: UpdateModel):
     return (
@@ -53,7 +53,7 @@ async def update_route(user_id: uuid.UUID, update_data: UpdateModel):
     summary='Retrieve User Notification Settings', 
     description='This should run when you enter a user\'s profile allowing them to see their notifications settings', 
     status_code=status.HTTP_200_OK, 
-    response_model=GetSettingsModel
+    response_model=NotificationSettingsModel
     )
 async def get_settings_route(user_id: uuid.UUID): 
     return (
@@ -64,7 +64,7 @@ async def get_settings_route(user_id: uuid.UUID):
     summary='Updates user\'s notification settings',
     description='This should run when a user makes a change to any of their notification settings within their account profile',
     status_code=status.HTTP_200_OK, 
-    response_model=GetSettingsModel
+    response_model=NotificationSettingsModel
     )
 async def update_settings_route(user_id: uuid.UUID, update_data: UpdateSettingsModel):
     return (
@@ -78,7 +78,7 @@ async def update_settings_route(user_id: uuid.UUID, update_data: UpdateSettingsM
     summary='Create a settings record on creation of a new user',
     description='This runs when a new user profile is created. It creates a record in the settings table with their specific settings. All are True by default',
     status_code=status.HTTP_200_OK, 
-    response_model=GetSettingsModel
+    response_model=NotificationSettingsModel
     )
 async def create_settings_controller(user_id: uuid.UUID, create_data: CreateSettingsModel):
     return (

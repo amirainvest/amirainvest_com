140628988c0fada353dd849386bcbfd29281b991
mountain <Fiveoat@gmail.com>
2022-02-03 15:31:09 -0700

    other updates,

--- src/backend_amirainvest_com/test/unit/backend_amirainvest_com/api/backend/bookmark/test_bookmark.py
+++ src/backend_amirainvest_com/test/unit/backend_amirainvest_com/api/backend/bookmark/test_bookmark.py
@@ -1,6 +1,5 @@
 import json
 from datetime import datetime
-from random import randint
 
 from fastapi import status
 from httpx import AsyncClient
@@ -9,7 +8,6 @@ from sqlalchemy.future import select
 
 from backend_amirainvest_com.api.app import app
 from common_amirainvest_com.schemas.schema import Bookmarks, Users
-from common_amirainvest_com.utils.test.factories.schema import BookmarksFactory, PostsFactory, UsersFactory
 
 from ...config import AUTH_HEADERS
 
@@ -22,39 +20,44 @@ async def test_auth():
     assert response.json() == {"detail": "Not authenticated"}
 
 
-async def test_list():
-    post_bookmarker = await UsersFactory()
-    post_creator = await UsersFactory()
-    post = await PostsFactory(creator_id=post_creator.id)
-    bookmark = await BookmarksFactory(user_id=post_bookmarker.id, post_id=post.id)
+async def test_list(factory):
+
+    post_bookmarker = await factory.gen("users")
+    post_creator = await factory.gen("users")
+    post = await factory.gen("posts", {"posts": {"creator_id": post_creator["users"].id}})
+    bookmark = await factory.gen(
+        "bookmarks", {"bookmarks": {"user_id": post_bookmarker["users"].id, "post_id": post["posts"].id}}
+    )
 
     async with AsyncClient(app=app, base_url="http://test") as async_client:
-        response = await async_client.post("/bookmark/list", params={"user_id": bookmark.user_id}, headers=AUTH_HEADERS)
+        response = await async_client.post(
+            "/bookmark/list", params={"user_id": bookmark["bookmarks"].user_id}, headers=AUTH_HEADERS
+        )
 
     assert response.status_code == status.HTTP_200_OK
     response_data = response.json()
 
     results = response_data["results"]
     assert type(results) == list
-    assert results[0]["user_id"] == str(post_bookmarker.id)
-    assert results[0]["post_id"] == post.id
+    assert results[0]["user_id"] == str(post_bookmarker["users"].id)
+    assert results[0]["post_id"] == post["posts"].id
     assert results[0]["is_deleted"] is False
 
 
-async def test_create(async_session_maker_test):
+async def test_create(async_session_maker_test, factory):
     session_test: AsyncSession = async_session_maker_test()
 
-    post_bookmarker = await UsersFactory()
-    post_creator = await UsersFactory()
-    post = await PostsFactory(creator_id=post_creator.id, id=randint(0, 10000))
+    post_bookmarker = await factory.gen("users")
+    post_creator = await factory.gen("users")
+    post = await factory.gen("posts", {"posts": {"creator_id": post_creator["users"].id}})
 
     async with AsyncClient(app=app, base_url="http://test") as async_client:
         response = await async_client.post(
             "/bookmark/create",
-            params={"user_id": post_bookmarker.id},
+            params={"user_id": post_bookmarker["users"].id},
             data=json.dumps(
                 {
-                    "post_id": post.id,
+                    "post_id": post["posts"].id,
                     "created_at": str(datetime.utcnow()),
                     "updated_at": str(datetime.utcnow()),
                     "is_deleted": False,
@@ -64,33 +67,37 @@ async def test_create(async_session_maker_test):
         )
         assert response.status_code == status.HTTP_201_CREATED
         response_data = response.json()
-        assert response_data["user_id"] == str(post_bookmarker.id)
-        assert response_data["post_id"] == post.id
+        assert response_data["user_id"] == str(post_bookmarker["users"].id)
+        assert response_data["post_id"] == post["posts"].id
         assert response_data["is_deleted"] is False
-        users = await session_test.execute(select(Users).where(Users.id == post_bookmarker.id))
+        users = await session_test.execute(select(Users).where(Users.id == post_bookmarker["users"].id))
         users = users.scalars().all()
-        assert post_bookmarker.id in [x.id for x in users]
+        assert post_bookmarker["users"].id in [x.id for x in users]
 
 
-async def test_delete(async_session_maker_test):
+async def test_delete(async_session_maker_test, factory):
     session_test: AsyncSession = async_session_maker_test()
 
-    post_bookmarker = await UsersFactory()
-    post_creator = await UsersFactory()
-    post = await PostsFactory(creator_id=post_creator.id)
-    bookmark = await BookmarksFactory(user_id=post_bookmarker.id, post_id=post.id)
+    post_bookmarker = await factory.gen("users")
+    post_creator = await factory.gen("users")
+    post = await factory.gen("posts", {"posts": {"creator_id": post_creator["users"].id}})
+    bookmark = await factory.gen(
+        "bookmarks", {"bookmarks": {"user_id": post_bookmarker["users"].id, "post_id": post["posts"].id}}
+    )
 
     async with AsyncClient(app=app, base_url="http://test") as async_client:
         response = await async_client.post(
             "/bookmark/delete",
-            params={"user_id": post_bookmarker.id, "bookmark_id": bookmark.id},
+            params={"user_id": post_bookmarker["users"].id, "bookmark_id": bookmark["bookmarks"].id},
             headers=AUTH_HEADERS,
         )
 
     assert response.status_code == status.HTTP_200_OK
 
-    user_bookmarks = await session_test.execute(select(Bookmarks).where(Bookmarks.user_id == post_bookmarker.id))
-    assert bookmark.id not in [x.id for x in user_bookmarks]
+    user_bookmarks = await session_test.execute(
+        select(Bookmarks).where(Bookmarks.user_id == post_bookmarker["users"].id)
+    )
+    assert bookmark["bookmarks"].id not in [x.id for x in user_bookmarks]
     assert len(list(user_bookmarks)) == 0
 
 

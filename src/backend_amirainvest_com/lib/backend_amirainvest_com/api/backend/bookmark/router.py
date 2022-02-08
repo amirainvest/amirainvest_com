9c306ba556318fa7968550ecb2b0bd668b8cfda2
jhirschibar <jhirschibar@gmail.com>
2022-02-08 09:34:13 -0500

    initial changes to auth

--- src/backend_amirainvest_com/lib/backend_amirainvest_com/api/backend/bookmark/router.py
+++ src/backend_amirainvest_com/lib/backend_amirainvest_com/api/backend/bookmark/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Security, status
+from fastapi import APIRouter, Depends, status
 
 from backend_amirainvest_com.api.backend.bookmark.controller import (
     create_controller,
@@ -6,16 +6,15 @@ from backend_amirainvest_com.api.backend.bookmark.controller import (
     list_controller,
 )
 from backend_amirainvest_com.api.backend.bookmark.model import CreateModel, GetModel, ListModel
-from backend_amirainvest_com.controllers.auth import auth_dep
+from backend_amirainvest_com.controllers.auth import auth_depends_user_id
 
 
-router = APIRouter(prefix="/bookmark", tags=["Bookmark"], dependencies=[Security(auth_dep, scopes=[])])
+router = APIRouter(prefix="/bookmark", tags=["Bookmark"])
 
 
 @router.post("/list", status_code=status.HTTP_200_OK, response_model=ListModel)
-async def list_route(
-    user_id: str,
-):
+async def list_route(token=Depends(auth_depends_user_id)):
+    user_id = token["https://amirainvest.com/user_id"]
     user_bookmarks = await list_controller(user_id)
     user_bookmarks = [x.dict() for x in user_bookmarks]
     return {"results": user_bookmarks, "result_count": len(user_bookmarks)}
@@ -23,9 +22,10 @@ async def list_route(
 
 @router.post("/create", status_code=status.HTTP_201_CREATED, response_model=GetModel)
 async def create_route(
-    user_id: str,
     bookmark_data: CreateModel,
+    token=Depends(auth_depends_user_id)
 ):
+    user_id = token["https://amirainvest.com/user_id"]
     bookmark = await create_controller(user_id, bookmark_data)
     bookmark = bookmark.dict()
     return bookmark
@@ -33,8 +33,9 @@ async def create_route(
 
 @router.post("/delete", status_code=status.HTTP_200_OK)
 async def delete_route(
-    user_id: str,
     bookmark_id: int,
+    token=Depends(auth_depends_user_id)
 ):
+    user_id = token["https://amirainvest.com/user_id"]
     await delete_controller(user_id, bookmark_id)
     return 200
--- src/backend_amirainvest_com/lib/backend_amirainvest_com/api/backend/husk_request/router.py
+++ src/backend_amirainvest_com/lib/backend_amirainvest_com/api/backend/husk_request/router.py
@@ -1,16 +1,17 @@
-from fastapi import APIRouter, Security, status
+from fastapi import APIRouter, Depends, status
 
 from backend_amirainvest_com.api.backend.husk_request.controller import create_controller, delete_controller
 from backend_amirainvest_com.api.backend.husk_request.model import CreateModel, GetModel
-from backend_amirainvest_com.controllers.auth import auth_dep
+from backend_amirainvest_com.controllers.auth import auth_depends_user_id
 
 
-router = APIRouter(prefix="/husk_request", tags=["Husk Request"], dependencies=[Security(auth_dep, scopes=[])])
+router = APIRouter(prefix="/husk_request", tags=["Husk Request"])
 
 
 @router.post("/create", status_code=status.HTTP_201_CREATED, response_model=GetModel)
 async def create_route(
     husk_request_data: CreateModel,
+    token=Depends(auth_depends_user_id)
 ):
     husk_request = await create_controller(husk_request_data)
     husk_request = husk_request.dict()
@@ -18,5 +19,7 @@ async def create_route(
 
 
 @router.post("/delete", status_code=status.HTTP_200_OK)
-async def delete_route(husk_request_id: int):
+async def delete_route(
+    husk_request_id: int,
+    token=Depends(auth_depends_user_id)):
     await delete_controller(husk_request_id)

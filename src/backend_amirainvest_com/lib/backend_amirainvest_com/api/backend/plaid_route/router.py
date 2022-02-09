ca36d96669fdd00676fecc23eae2f64290ef2915
David Bozentka <djbozentka@gmail.com>
2022-02-09 11:15:03 -0800

    Easier plaid testing integration

--- src/backend_amirainvest_com/lib/backend_amirainvest_com/api/backend/plaid_route/router.py
+++ src/backend_amirainvest_com/lib/backend_amirainvest_com/api/backend/plaid_route/router.py
@@ -9,27 +9,34 @@ from common_amirainvest_com.dynamo.utils import add_brokerage_user, get_brokerag
 from common_amirainvest_com.sqs.consts import BROKERAGE_DATA_QUEUE_URL
 from common_amirainvest_com.sqs.models import Brokerage, BrokerageDataActions, BrokerageDataChange
 from common_amirainvest_com.sqs.utils import add_message_to_queue
+from backend_amirainvest_com.api.backend.plaid_route.models import PlaidTokenRequest
 
 
 router = APIRouter(prefix="/plaid", tags=["Plaid"])
 templates = Jinja2Templates(directory="src/backend_amirainvest_com/lib/backend_amirainvest_com/api/backend/plaid_route")
 
 
-@router.get("/link", status_code=status.HTTP_200_OK, response_class=HTMLResponse)
-async def get_link(request: Request, user_id: str, token=Depends(auth_depends_user_id)):
+@router.get('/', status_code=status.HTTP_200_OK)
+async def get_plaid(request: Request):
+    return templates.TemplateResponse('login.html', {'request': request})
+
+
+@router.post("/link", status_code=status.HTTP_200_OK, response_class=HTMLResponse)
+async def get_link(token=Depends(auth_depends_user_id)):
+    user_id = token["https://amirainvest.com/user_id"]
     link_token = generate_link_token(user_id)
-    return templates.TemplateResponse("link.html", {"request": request, "link_token": link_token})
+    return JSONResponse({"link_token": link_token})
 
 
 # TODO Could move most of the logic to controller
-@router.post("/link", status_code=status.HTTP_200_OK, response_class=JSONResponse)
-async def post_link(user_id: str, public_token: str = Form(...), token=Depends(auth_depends_user_id)):
+@router.post("/token", status_code=status.HTTP_200_OK, response_class=JSONResponse)
+async def get_token(token_request: PlaidTokenRequest, token=Depends(auth_depends_user_id)):
+    public_token = token_request.public_token
+    user_id = token["https://amirainvest.com/user_id"]
     exchange_response = exchange_public_for_access_token(public_token)
     access_token = exchange_response["access_token"]
     item_id = exchange_response["item_id"]
-
     brokerage_user = await get_brokerage_user(user_id)
-
     if brokerage_user is None:
         await add_brokerage_user(
             BrokerageUser(

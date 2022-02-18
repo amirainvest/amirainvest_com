b9b5768536ae34dc3a64c96abdf89bf04238f95b
mountain <Fiveoat@gmail.com>
2022-02-18 10:46:32 -0700

    refactored response

--- src/backend_amirainvest_com/lib/backend_amirainvest_com/api/backend/discovery/controller.py
+++ src/backend_amirainvest_com/lib/backend_amirainvest_com/api/backend/discovery/controller.py
@@ -1,3 +1,4 @@
+import itertools
 from typing import List
 
 from sqlalchemy import func, select
@@ -16,6 +17,8 @@ async def get_controller(session: AsyncSession) -> List[GetModel]:
         await session.execute(
             (
                 select(Users, func.count(schema.UserSubscriptions.creator_id).label("subscription_count"))
+                .where(Users.trading_strategies.isnot(None))  # type: ignore
+                .where(Users.public_profile_deactivate != True)  # noqa
                 .outerjoin(UserSubscriptions, UserSubscriptions.creator_id == Users.id)
                 .group_by(UserSubscriptions.id, Users.id)
                 .order_by("subscription_count")
@@ -23,7 +26,16 @@ async def get_controller(session: AsyncSession) -> List[GetModel]:
         )
     ).all()
     for user, subscription_count in data:
-        profile = user.dict()
-        profile["subscription_count"] = subscription_count
-        discovery_profiles.append(GetModel(**profile))
-    return discovery_profiles
+        user = user.dict()
+        discovery_profiles.append(
+            {
+                "creator": user,
+                "subscription_count": subscription_count,
+                "trading_strategy": user["trading_strategies"][0] if user["trading_strategies"] else "",
+            }
+        )
+    discovery_profiles = sorted(discovery_profiles, key=lambda x: x["trading_strategy"])
+    return [
+        GetModel(**{"trading_strategy": trading_strategy, "values": list(values)})
+        for trading_strategy, values in itertools.groupby(discovery_profiles, key=lambda x: x["trading_strategy"])
+    ]
--- src/backend_amirainvest_com/test/unit/backend_amirainvest_com/api/backend/discovery/test_discovery.py
+++ src/backend_amirainvest_com/test/unit/backend_amirainvest_com/api/backend/discovery/test_discovery.py
@@ -8,8 +8,9 @@ from ...config import AUTH_HEADERS
 
 async def test_get(factory):
     num_users = 4
+    await factory.gen("users", {"users": {"trading_strategies": ["Long/Short"], "public_profile_deactivate": True}})
     for _ in range(0, num_users):
-        await factory.gen("users")
+        await factory.gen("users", {"users": {"trading_strategies": ["Long/Short"]}})
     async with AsyncClient(app=app, base_url="http://test") as async_client:
         response = await async_client.post(
             "/discovery/get",
@@ -17,6 +18,6 @@ async def test_get(factory):
         )
     assert response.status_code == status.HTTP_200_OK
     response_data = response.json()
-    assert len(response_data) == num_users
-    for profile in response_data:
-        assert profile["subscription_count"] == 0
+    assert len(response_data) == 1
+    assert response_data[0]["trading_strategy"] == "Long/Short"
+    assert len(response_data[0]["values"]) == num_users

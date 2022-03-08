b01af785a59fef1ad4bce001ae01b0dcd6172215
David Bozentka <djbozentka@gmail.com>
2022-03-07 16:48:39 -0800

    Fmt

--- terraform/modules/brokerage_data_lambda/dynamodb_table.tf
+++ terraform/modules/brokerage_data_lambda/dynamodb_table.tf
@@ -10,28 +10,28 @@ resource "aws_dynamodb_table" "brokerage_users" {
   }
 
   billing_mode = "PAY_PER_REQUEST"
-  hash_key = "user_id"
-  range_key = "item_id"
-  name = "brokerage_users"
+  hash_key     = "user_id"
+  range_key    = "item_id"
+  name         = "brokerage_users"
 
   point_in_time_recovery {
     enabled = "false"
   }
 
   stream_enabled = "false"
-  table_class = "STANDARD"
+  table_class    = "STANDARD"
 
   tags = {
-    env = "prod"
+    env     = "prod"
     project = local.project
   }
 
   tags_all = {
-    env = "prod"
+    env     = "prod"
     project = local.project
   }
 
-  read_capacity = "0"
+  read_capacity  = "0"
   write_capacity = "0"
 
   lifecycle {
